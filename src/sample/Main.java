97ae5a2f612caaee4e2ab42bf1158d525eda487f
Mopahta <novoseletskiy03@mail.ru>
2022-03-05 10:45:22 +0300

    drawing figures in javafx

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,17 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage){
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Drawing Operations");
 
